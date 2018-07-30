c8504d92ee8962242a02926070c87299b7cf4386
Jordan Parker <parkerj4321@gmail.com>
2018-07-30 15:47:31 -0500

    Updated exercise11

--- Exercise11.js
+++ Exercise11.js
@@ -1,12 +1,12 @@
 var grade = parseInt(prompt("Please enter your grade"));
 
-if(grade <=100 && grade > 90 ){
+if(grade <=100 && grade >= 90 ){
     console.log("You got A")
-}else if(grade <90  && grade > 80){
+}else if(grade <90  && grade >= 80){
     console.log("You got a B");
-}else if(grade < 80 && grade > 70){
+}else if(grade < 80 && grade >= 70){
     console.log("You got a C");
-}else if(grade < 70 && grade > 60){
+}else if(grade < 70 && grade >= 60){
     console.log("You got a D");
 }else if(grade <60){
     console.log("BOI");
